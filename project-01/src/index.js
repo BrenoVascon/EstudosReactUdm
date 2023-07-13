7c84ec37cb669e4b01544fe879d39d42188b4b1a
Breno Vasc <beniolimavasc@gmail.com>
2023-07-12 23:28:47 -0300

    Estudos

--- project-01/src/index.js
+++ project-01/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import '../src/components/styles/global.css';
+import Home from '../src/components/Pages/Home';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <Home/>
   </React.StrictMode>
 );
 
